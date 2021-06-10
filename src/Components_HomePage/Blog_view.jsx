7df5c0a8b2cfedb18fb9e8781e7fcb4f58276fe5
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2021-06-10 12:15:47 +0530

    blog prototype added

--- src/Components_HomePage/Blog_view.jsx
+++ src/Components_HomePage/Blog_view.jsx
@@ -1,64 +1,68 @@
 import React from "react";
 import "./Blog_view.css";
+import {Link, Links} from "react-router-dom";
 
 function Blog_view()
 {
     return (
         <div>
-            <h1>Learning Stuff</h1>
-            <div className="container">
+            
 
-            <div class="row">
-    <div class="col-sm">
-    <div class="card" >
-  <img class="card-img-top" src="/images/dart.png" alt="Card image cap"/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
+      <div class="containerr">
+      <h1>Blog</h1>
+  <main class="grid">
+    <article>
+      <img src="/images/app.jpg" alt="Sample photo"/>
+      <div class="text">
+        <h3>App Development</h3>
+        <p>Mobile app development is the act or process by which a mobile app is developed for mobile devices, such as personal digital assistants, enterprise digital assistants or mobile phones.</p>
+        <Link to="/blog">
+        <a href="#" class="btn btn-primary btn-block mybt">Let's go</a>
+      </Link>  
+      
+      </div>
+    </article>
+    <article>
+      <img src="/images/web.png" alt="Sample photo"/>
+      <div class="text">
+        <h3>Web Development</h3>
+          <p>Web development is the work involved in developing a Web site for the Internet or an intranet.</p>
+          <Link to="/blog">
+          <a href="#" class="btn btn-primary btn-block mybt">Let's go</a>
 
+          </Link>    
+      
+      </div>
+    </article>
+    <article>
+      <img src="/images/pro.png" alt="Sample photo"/>
+      <div class="text">
+        <h3>Programming Languages</h3>
+        <p>.A programming language is a computer language that is used by programmers (developers) to communicate with computers. It is a set of instructions written in any specific language ( C, C++, Java, Python) to perform a specific task.</p>
+        <Link to="/blog">
+        <a href="#" class="btn btn-primary btn-block mybt">Let's go</a>
 
-    <div class="col-sm">
-    <div class="card" >
-  <img class="card-img-top" src="/images/flutter.jpg" alt="Card image cap"/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
-
-
-    <div class="col-sm">
-    <div class="card" >
-     <img class="card-img-top"
-   src="/images/dart.png"
-    alt="Card image cap"
-
-    />
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
-
-
-
+        </Link>
+      
+      </div>
+    </article>
     
-    
-  </div>
+  
+  </main>
+</div>
 
 
-        
-        
-        </div>
-        </div>
+
+
+
+              
+          </div>
+
+
+
+
+       
+
     );
 
 }
