660df6927653376a297c59c25f0c7c9f3d6fa70c
akkav <akka9936@gmail.com>
2021-06-20 16:45:42 +0530

    resources page done

--- src/Pages/Resources.jsx
+++ src/Pages/Resources.jsx
@@ -1,6 +1,47 @@
 import React from "react";
+import './resources.css';
 function Resources()
 {
-    return <h1>I am Resources</h1>
+    return (
+        <>
+        <h1 className='heads' >Resources</h1>
+        <div className='underline'></div>
+        <div className=' mains' >
+            <h1 className='heading'>Roadmap</h1>
+            <div>
+  <ol>
+    <a href='' target='_blank' > <li><span>1.</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur.  </p></li></a>
+    <a href='' target='_blank' > <li><span>2.</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur.  </p></li></a>
+    <a href='' target='_blank' > <li><span>3.</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur.  </p></li></a>
+  </ol> 
+</div>
+            
+        </div>
+        <div className=' mains' >
+            <h1 className='heading'>Notes</h1>
+            <div>
+  <ol>
+  <a href='' target='_blank' > <li><span>1.</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur.  </p></li></a>
+  <a href='' target='_blank' > <li><span>2.</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur.  </p></li></a>
+  <a href='' target='_blank' > <li><span>3.</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur.  </p></li></a>
+  </ol> 
+</div>
+            
+        </div>
+        <div className=' mains' >
+            <h1 className='heading'>internship</h1>
+            <div>
+  <ol>
+  <a href='' target='_blank' > <li><span>1.</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur.  </p></li></a>
+  <a href='' target='_blank' > <li><span>2.</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur.  </p></li></a>
+  <a href='' target='_blank' > <li><span>3.</span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur.  </p></li></a>
+
+  </ol> 
+</div>
+            
+        </div>
+        
+        </>
+    )
 }
 export default Resources;
